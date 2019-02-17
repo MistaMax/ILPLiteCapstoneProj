b2de12a8e9ad16ed72bb59305484f147f17d1bbb
MistaMax <mmctheace@gmail.com>
2019-02-17 13:43:22 -0600

    I added the basic config txt model for the demo and started on the file io stream scanning

--- ILPLite.cpp
+++ ILPLite.cpp
@@ -1,11 +1,15 @@
+#include <iostream>
+#include <fstream>
 
+using namespace std;
 
-
-
-
-
-
-int main(){
-
+int main(int argc, char *argv[])
+{
+	ifstream matrixFile;
+	string input;
+	matrixFile.open("input.txt");
+	matrixFile >> input;
+	cout << input << endl;
+	matrixFile.close();
 	return 0;
 }
